670c1ff93f4277d1f7f36f86de76208055558c67
Eyeball Edward <edwardyeung.private@gmail.com>
2016-12-26 17:02:38 +0800

    Use 302 so testing w/o https still works

--- request/http-redirect.js
+++ request/http-redirect.js
@@ -1,6 +1,6 @@
 module.exports = function(serverLocation) {
     return function(req, res) {
-        res.writeHead(301, {
+        res.writeHead(302, {
             'Location': serverLocation
         });
         res.end();
